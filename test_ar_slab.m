32bde94c8aa34c7237bb1f21425d2d0914ac2f54
gtmelgar <gtmelgar@gmail.com>
2025-07-29 22:40:54 -0400

    modification of test file, adding additional ar test case

--- test_ar_slab.m
+++ test_ar_slab.m
@@ -1,40 +1,59 @@
 % example simulaition 
-% 1foot slab (30.48cm) ur = 2 , e_r = 6 with air around it
-% test R and T from 0 to 1GHz
+% 1foot slab (30.48cm) ur = 1 , e_r = 12 with air around it
+% apply AR coating to allow 2.4GHz transmission
+
+% Assume no magnetic response 
+% AR coating thickness should be 1/4 wavelength
+
 close all
-
+clear 
 addpath('./src')
-
 param = getDefaultParameters();
-param.nzsrc = 2;
-param.spacerRegion = 10;
-param.num_plot = 10;
-param.num_bins = 100;
-f_Hz = (0.1:0.1:1)*1e9;
-
-u_r = 2;
-e_r = 6;
-n_max = sqrt(u_r*e_r);
-
+param.spacerRegion = 100;
+param.num_plot = 100;
+param.num_bins = 500;
+f_Hz = (0.1:0.1:5)*1e9;
 lambda = param.const.c0./f_Hz;
 
-dmin = 30.48e-2; % slab to simulate
+er_1 = 12;
+er_air = 1;
 
-[param] = get_resolution(param, lambda, dmin, n_max);
+n1 = sqrt(er_1*1);
+
+% calculate geometric mean
+er_2 = sqrt(er_1*er_air); 
+
+% calculate ref. index
+n2 = sqrt(er_2);
+
+lambda0 = param.const.c0./2.4e9;
+d2 = lambda0./(4*n2);
+
+
+dmin = min(30.48e-2,d2); % slab to simulate
+[param] = get_resolution(param, lambda, dmin, max(n1,n2));
 % calculate total number of points for simulation
-param.Nz = param.N + 2*param.spacerRegion + 3; % 3 comes from T R and source cells
+param.Nz = param.N*2 + round(30.48e-2./param.dz) + 2*param.spacerRegion + 3; % 3 comes from T R and source cells
 
 % material properties
 ER = ones(1,param.Nz);
 UR = ones(1,param.Nz);
 
 % calculate start and end points of slab
-Nz1 = param.spacerRegion + 2;
-Nz2 = Nz1 + param.N - 1;
+Nz1 = param.spacerRegion + 2; % start of AR coating
+Nz2 = Nz1 + param.N; % end of AR coating
+Nz3 = Nz2 + round(30.48e-2./param.dz); % material + start 2nd set of AR coating
+Nz4 = Nz3 + param.N; %end 2nd set of AR coating
 
 % create structure
-UR(Nz1:Nz2) = u_r;
-ER(Nz1:Nz2) = e_r;
+UR(Nz1:Nz2) = 1;
+ER(Nz1:Nz2) = er_2;
+
+UR(Nz2:Nz3) = 1;
+ER(Nz2:Nz3) = er_1;
+
+UR(Nz3:Nz4) = 1;
+ER(Nz3:Nz4) = er_2;
 
 % create ref index structure
 n_matrix = sqrt(UR.*ER);
@@ -42,13 +61,14 @@ n_matrix = sqrt(UR.*ER);
 % compute min time step
 param.dt = min(n_matrix).*param.dz/(2*param.const.c0);
 
+
 % generate gaussian pulse and delay by 6tau
 fmax = param.const.c0/min(lambda);
 tau = 0.5./fmax;
 t0 = 6*tau;
 
 % get num of time steps to solve for
-t_prop = n_max*param.Nz*param.dz./param.const.c0;
+t_prop = max(n_matrix)*param.Nz*param.dz./param.const.c0;
 
 sim_time = 12.*tau + 5 * t_prop;
 num_steps = ceil(sim_time./param.dt);
